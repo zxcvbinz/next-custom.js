d1594ebf8f8395482320a5279cd4e608edf0cc44
zxcvbinz <davide.lanotte.unix@gmail.com>
2023-02-24 16:50:30 +0100

    fix, repo error

--- src/client/components/router-reducer/create-href-from-url.ts
+++
@@ -1,5 +0,0 @@
-export function createHrefFromUrl(
-  url: Pick<URL, 'pathname' | 'search' | 'hash'>
-): string {
-  return url.pathname + url.search + url.hash
-}
--- src/lib/metadata/types/resolvers.ts
+++
@@ -1,9 +0,0 @@
-import { Metadata, ResolvedMetadata } from './metadata-interface'
-
-export type FieldResolver<Key extends keyof Metadata> = (
-  T: Metadata[Key]
-) => ResolvedMetadata[Key]
-export type FieldResolverWithMetadataBase<Key extends keyof Metadata> = (
-  T: Metadata[Key],
-  metadataBase: ResolvedMetadata['metadataBase']
-) => ResolvedMetadata[Key]
